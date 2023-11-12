f7bf7c254dc8576a048d18dd0a278f36bdd59d67
Nitrite19 <77036238+Nitrite19@users.noreply.github.com>
2023-11-12 14:12:58 -0800

    Added file
    This is phase 1 of servo motor control. This simply controls two LEDs that are given as 30% and 70% duty cycle.

---
+++ SamPrototype/main.c
@@ -0,0 +1,35 @@
+/*
+ * Gimbal PWM.c
+ *
+ * Created: 11/09/2023 9:36:10 PM
+ * Author : RASC-GULL
+ */ 
+
+//Objective is to control LED's and 360 Servos with pulse width modulation
+
+#include <avr/io.h>
+#include <time.h>
+#include <avr/interrupt.h>
+
+void init()
+{
+	DDRD  |= (1<<DDD5) | (1<<DDD6); //Enable pin outputs D5 and D6
+	PORTD |= (1<<5) | (1<<6);
+}
+
+void timer_0_init()
+{
+	TCCR0A |= (1<<WGM00) | (1<<WGM01) | (1<<COM0A1); //Enable Fast PWM for Timer0 LED
+	TCCR0B |= (1<<WGM02) | (1<<CS00) | (1<<CS02); //Pre-Scale to 1024
+	OCR0A = 77; //30% duty cycle of pre-scale
+	OCR0B = 179; //70% duty cycle of pre-scale
+}
+
+int main(void)
+{	
+	init();
+	timer_0_init();
+
+    while (1){}
+}
+
